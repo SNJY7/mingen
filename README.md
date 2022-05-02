e8649799f7707c95be37453ac44728fea4c77d8b
Sanjay V S <sanjayvs2004@gmail.com>
2022-05-03 00:33:05 +0530

    title and header added

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 Minimal Static Site Generator
 
-# HOW TO USE
+# How to use
 
 1. Copy mingen.py into your website's main folder.
 2. make a folder named 'md' and store all your markdown files here.
 3. Run mingen.py
 
 All markdown files and subfolders inside the folder 'md' will be recursively generated in your website folder.
+The first line will be used as the title and heading.
 
+# Test
+
+Copy the mingen.py file to the folder named 'demo' and run the script.
--- mingen.py
+++ mingen.py
@@ -2,8 +2,8 @@
 # created on: Sunday 17 April 2022 03:31:45 PM IST
 
 import os
-import markdown
 import sys
+import markdown
 
 cwd = os.getcwd()
 
@@ -12,14 +12,16 @@ htmlPath = cwd
 
 def md2html(mdFilePath, mdFileName):
     mdFile_ = open(mdFilePath+"/"+mdFileName, "r")
+    firstLine = mdFile_.readline()
     mdFile = mdFile_.read()
     htmlPath = mdFilePath.replace("/md", "", 1)
     if os.path.isdir(htmlPath) != True:
         os.mkdir(htmlPath)
     htmlFile_ = open(htmlPath+"/"+mdFileName[:-3]+".html", "w+")
-    css = "<link rel=\"stylesheet\" href=\"/main.css\">\n"
-    head = "<head>\n" + "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" + css + "\n</head>\n"
-    body = "<body>\n" + markdown.markdown(mdFile) + "</body>\n"
+    css = "<link rel=\"stylesheet\" href=\"main.css\">\n"
+    title = "<title>" + firstLine[2:] + "</title>"
+    head = "<head>\n" + "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" + title + css + "</head>\n"
+    body = "<body>\n" + "<header>" + markdown.markdown(firstLine) + "</header>" + markdown.markdown(mdFile) + "\n</body>\n"
     htmlFile = "<html>\n" + head + body + "</html>"
     htmlFile_.write(htmlFile)
     mdFile_.close()
@@ -33,4 +35,4 @@ def compile(filePath):
         elif fileName[-3:] == ".md":
             md2html(filePath, fileName)
 
-compile(mdPath)
+compile(cwd)
